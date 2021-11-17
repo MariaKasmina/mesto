5ff97a0fedca772fdf9a58f3a1ba3cd5c4caca84
Maria Kazmina <maryamm2017amm@yandex.ru>
2021-11-17 18:32:44 +0300

    added template for cards

---
+++ scripts/template.js
@@ -0,0 +1,35 @@
+const cardContainer = document.querySelector('.elements');
+const addButton = document.querySelector('.profile__add-button');
+
+import {initialCards} from "./data/data_for_template.js";
+
+function addCard(title, url) {
+    const cardTemplate = document.querySelector('#element-template').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+    cardElement.querySelector('.element__caption-text').textContent = title;
+    cardElement.querySelector('.element__image').src = url;
+
+    cardElement.querySelector('.element__heart').addEventListener('click', function(evt) {
+        // console.log(evt.target)
+        // const eventTarget = evt.target;
+        // eventTarget.classList.toggle('song__like_active');
+    });
+
+    cardContainer.append(cardElement);
+}
+
+initialCards.map((card, index) => {
+    addCard(card.name, card.link);
+});
+
+addButton.addEventListener('click', function () {
+   /* const artist = document.querySelector('.input__text_type_artist');
+    const title = document.querySelector('.input__text_type_title');
+
+    addCard(artist.value, title.value);
+    renderHasSongs();
+
+    artist.value = '';
+    title.value = '';*/
+});
