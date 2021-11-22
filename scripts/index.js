d0083876307736428752fe661be2d4c8504f1855
Maria Kazmina <maryamm2017amm@yandex.ru>
2021-11-22 22:40:39 +0300

    fix selectors and callbacks

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,8 @@ const userProfession = document.querySelector('.profile__info-description'); // 
 const editBtn = document.querySelector('.profile__edit-button'); // кнопка Изменить в хедере
 const changePersonalInfoPopUp = document.querySelector('.popup_change_personal-info'); // поп-ап изменения данных профиля
 const addNewPlacePopUp = document.querySelector('.popup_add_new-place'); // поп-ап добавления карточки
-const changePersonalInfoForm = document.querySelector('.popup_change_personal-info .form'); // форма изменения данных пользователя
-const addNewLocationForm = document.querySelector('.popup_add_new-place .form'); // форма добавления новой карточки
+const changePersonalInfoForm = document.querySelector('.form_type_edit'); // форма изменения данных пользователя
+const addNewLocationForm = document.querySelector('.form_type_add'); // форма добавления новой карточки
 const nameInput = document.querySelector('#name'); // поле для ввода имени
 const professionInput = document.querySelector('#additionalInfo'); // поле для ввода профессии
 const place = addNewLocationForm.querySelector('#place'); // поле для ввода названия карточки
@@ -21,10 +21,41 @@ const popUpWithImageCloseBtn = popUpWithImage.querySelector('.popup__close-butto
 /**
  * Функция для действий по открытию поп-апа изменения данных профиля
  */
-function openChangePersonalInfoPopup(element) {
-    element.classList.add('popup_opened');
+function openChangePersonalInfoPopup() {
     nameInput.value = userName.textContent;
     professionInput.value = userProfession.textContent;
+    openPopup(changePersonalInfoPopUp);
+}
+
+/**
+ * Функция для действий по открытию поп-апа добавления новой карточки
+ */
+function openAddNewPlacePopUp() {
+    place.value = '';
+    url.value = '';
+    openPopup(addNewPlacePopUp);
+}
+
+/**
+ * Функция для действий по отправке формы изменения данных пользователя
+ * @param evt
+ */
+function submitChangePersonalInfoForm(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    userName.textContent = nameInput.value;
+    userProfession.textContent = professionInput.value;
+    closePopup(changePersonalInfoPopUp);
+}
+
+/**
+ * Функция для действия по отправке формы добавления новой карточки
+ * @param evt
+ */
+function submitAddNewLocationForm(evt) {
+    evt.preventDefault();
+    renderCard(createCard(place.value, url.value), 'prepend');
+    closePopup(addNewPlacePopUp);
+    document.querySelector('.elements__no-items').style.display = 'none';
 }
 
 /**
@@ -38,16 +69,14 @@ editBtn.addEventListener('click', () => {
  * Обработка события "Клик на кнопку добавления новой карточки"
  */
 addNewLocationBtn.addEventListener('click', () => {
-    place.value = '';
-    url.value = '';
-    openPopup(addNewPlacePopUp);
+    openAddNewPlacePopUp();
 });
 
 /**
  * Обработка события "Клик на кнопку закрытия поп-апа изменения данных о пользователе"
  */
 closeChangePersonalInfoPopUpBtn.addEventListener('click', () => {
-   closePopup(changePersonalInfoPopUp);
+    closePopup(changePersonalInfoPopUp);
 });
 
 /**
@@ -60,22 +89,12 @@ closeAddNewPlacePopUpBtn.addEventListener('click', () => {
 /**
  * Обработка события "Отправка формы изменения данных пользователя"
  */
-changePersonalInfoForm.addEventListener('submit', (evt) => {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    userName.textContent = nameInput.value;
-    userProfession.textContent = professionInput.value;
-    closePopup(changePersonalInfoPopUp);
-});
+changePersonalInfoForm.addEventListener('submit', (evt) => submitChangePersonalInfoForm(evt));
 
 /**
  * Обработка события "Отпавка формы добавления новой карточки"
  */
-addNewLocationForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    renderCard(createCard(place.value, url.value), 'prepend');
-    closePopup(addNewPlacePopUp);
-    document.querySelector('.elements__no-items').style.display = 'none';
-});
+addNewLocationForm.addEventListener('submit', (evt) => submitAddNewLocationForm(evt));
 
 /**
  * Обработка события "Клик на кнопку закрытия поп-апа с картинкой"
