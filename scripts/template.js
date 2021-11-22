b64f0e58f8c1dbafd08b061e8319f55a9f7f3082
Maria Kazmina <maryamm2017amm@yandex.ru>
2021-11-22 18:08:02 +0300

    refactor cards, popups and scripts

--- scripts/template.js
+++ scripts/template.js
@@ -1,51 +1,85 @@
-const cardContainer = document.querySelector('.elements');
-const popUpWithImage = document.querySelector('.popup_with_image');
-
 import {initialCards} from "./data/data_for_template.js";
 import {openPopup} from "./popup.js";
 
-export function addCard(title, url) {
+const cardContainer = document.querySelector('.elements');
+const popUpWithImage = document.querySelector('.popup_with_image');
+
+/**
+ * Функция отрисовки карточек
+ * @param card элемент карточки
+ * @param placingMethod метод вставки карточки в контейнер: в конец, в начало и т.п.
+ */
+export function renderCard(card, placingMethod = 'append') {
+    switch (placingMethod) {
+        case 'append':
+            cardContainer.append(card);
+            break;
+        case 'prepend':
+            cardContainer.prepend(card);
+            break;
+        default:
+            throw new Error('Undefined placing elements method');
+    }
+}
+
+/**
+ * Функция создания эелемента карточки
+ * @param title заголовок
+ * @param url ссылка на картинку
+ * @returns {Node} объект карточка
+ */
+export function createCard(title, url) {
     const cardTemplate = document.querySelector('#element-template').content;
     const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
     cardElement.querySelector('.element__caption-text').textContent = title;
     cardElement.querySelector('.element__image').src = url;
 
-    cardElement.querySelector('.element__heart').addEventListener('click', function(evt) {
-        const eventTarget = evt.target;
-        eventTarget.classList.toggle('element__heart_active');
-    });
+    cardElement.querySelector('.element__heart').addEventListener('click', (evt => setHeartState(evt.target)));
 
-    cardElement.querySelector('.element__image').addEventListener('click', function (evt) {
-        const eventTarget = evt.target;
-        const image = popUpWithImage.querySelector('.popup__image');
-        image.src = eventTarget.getAttribute('src');
-        const desc = popUpWithImage.querySelector('.popup__image-description');
-        desc.textContent = cardElement.querySelector('.element__caption-text').textContent;
-        if (image.width > image.height) {
-            image.width = 816;
-            image.height = image.width / 1.51;
-        } else {
-            image.width = 433;
-            image.height = image.width * 1.25;
-        }
-        openPopup(popUpWithImage)
-    })
+    cardElement.querySelector('.element__image').addEventListener('click', () => {openImagePopUp(title, url)});
 
     cardElement.querySelector('.element__delete-btn').addEventListener('click', function () {
         cardElement.remove();
-        if(document.querySelector('.element') === null){
+        if (document.querySelector('.element') === null) {
             renderNoCards();
         }
     });
 
-    cardContainer.prepend(cardElement);
+    return cardElement;
 }
 
-initialCards.map((card, index) => {
-    addCard(card.name, card.link);
+/**
+ * Функция для переключения состояния сердечка
+ * @param target элемент, с которым взаимодействует пользователь
+ */
+function setHeartState(target) {
+    target.classList.toggle('element__heart_active');
+}
+
+/**
+ * Функция для открытия поп-апа с картинкой
+ * @param title заголовок
+ * @param url ссылка на картинку
+ */
+function openImagePopUp(title, url) {
+    const image = popUpWithImage.querySelector('.popup__image');
+    image.src = url;
+    const desc = popUpWithImage.querySelector('.popup__image-description');
+    desc.textContent = title;
+    openPopup(popUpWithImage);
+}
+
+/**
+ * Вывод на экран исходных карточек
+ */
+initialCards.map((card) => {
+    renderCard(createCard(card.name, card.link), 'append');
 });
 
-function renderNoCards(){
+/**
+ * Функция отрисовки текста "Добавьте карточку", когда блок с карточками пустой
+ */
+function renderNoCards() {
     document.querySelector('.elements__no-items').style.display = 'block';
 }
