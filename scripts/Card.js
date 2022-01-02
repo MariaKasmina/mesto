adddc9b32473d7ce4a250937848a0499cc341a64
Maria Kazmina <maryamm2017amm@yandex.ru>
2022-01-02 13:17:08 +0300

    added Card class

---
+++ scripts/Card.js
@@ -0,0 +1,51 @@
+import {openPopup} from "./popup.js";
+
+export class Card {
+    _popUpWithImage = document.querySelector('.popup_with_image');
+
+    constructor(title, url, desc, selector) {
+        this.title = title;
+        this.url = url;
+        this.description = desc;
+        this.selector = selector;
+    }
+
+    _setHeartState(target) {
+        target.classList.toggle('element__heart_active');
+    }
+
+    _openImagePopUp() {
+        const image = this._popUpWithImage.querySelector('.popup__image');
+        image.src = this.url;
+        image.alt = this.description;
+        const desc = this._popUpWithImage.querySelector('.popup__image-description');
+        desc.textContent = this.title;
+        openPopup(this._popUpWithImage);
+    }
+
+    _removeCard(element){
+        element.remove();
+        if (document.querySelector('.element') === null) {
+            this._renderNoCards();
+        }
+    }
+    _renderNoCards() {
+        document.querySelector('.elements__no-items').style.display = 'block';
+    }
+
+    createCard() {
+        const cardTemplate = document.querySelector(this.selector).content;
+        const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+        cardElement.querySelector('.element__caption-text').textContent = this.title;
+        cardElement.querySelector('.element__image').src = this.url;
+        cardElement.querySelector('.element__image').alt = this.description;
+
+        cardElement.querySelector('.element__heart').addEventListener('click', (evt => this._setHeartState(evt.target)));
+
+        cardElement.querySelector('.element__image').addEventListener('click', () => {this._openImagePopUp(this.title, this.url, this.description)});
+
+        cardElement.querySelector('.element__delete-btn').addEventListener('click', () => this._removeCard(cardElement));
+
+        return cardElement;
+    }
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,5 @@
 import {initialCards} from "./data/data_for_template.js";
-import {openPopup} from "./popup.js";
+import {Card} from "./Card.js";
 
 const cardContainer = document.querySelector('.elements');
 const popUpWithImage = document.querySelector('.popup_with_image');
@@ -23,67 +23,8 @@ export function renderCard(card, placingMethod = 'append') {
 }
 
 /**
- * Функция создания эелемента карточки
- * @param title заголовок
- * @param url ссылка на картинку
- * @param desc описание картинки
- * @returns {Node} объект карточка
- */
-export function createCard(title, url, desc) {
-    const cardTemplate = document.querySelector('#element-template').content;
-    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
-    
-    cardElement.querySelector('.element__caption-text').textContent = title;
-    cardElement.querySelector('.element__image').src = url;
-    cardElement.querySelector('.element__image').alt = desc;
-
-    cardElement.querySelector('.element__heart').addEventListener('click', (evt => setHeartState(evt.target)));
-
-    cardElement.querySelector('.element__image').addEventListener('click', () => {openImagePopUp(title, url, desc)});
-
-    cardElement.querySelector('.element__delete-btn').addEventListener('click', function () {
-        cardElement.remove();
-        if (document.querySelector('.element') === null) {
-            renderNoCards();
-        }
-    });
-
-    return cardElement;
-}
-
-/**
- * Функция для переключения состояния сердечка
- * @param target элемент, с которым взаимодействует пользователь
- */
-function setHeartState(target) {
-    target.classList.toggle('element__heart_active');
-}
-
-/**
- * Функция для открытия поп-апа с картинкой
- * @param title заголовок
- * @param url ссылка на картинку
- * @param description описание картинки
- */
-function openImagePopUp(title, url, description) {
-    const image = popUpWithImage.querySelector('.popup__image');
-    image.src = url;
-    image.alt = description;
-    const desc = popUpWithImage.querySelector('.popup__image-description');
-    desc.textContent = title;
-    openPopup(popUpWithImage);
-}
-
-/**
  * Вывод на экран исходных карточек
  */
 initialCards.map((card) => {
-    renderCard(createCard(card.name, card.link, card.desc), 'append');
+    renderCard(new Card(card.name, card.link, card.desc, '#element-template').createCard(), 'append');
 });
-
-/**
- * Функция отрисовки текста "Добавьте карточку", когда блок с карточками пустой
- */
-function renderNoCards() {
-    document.querySelector('.elements__no-items').style.display = 'block';
-}
